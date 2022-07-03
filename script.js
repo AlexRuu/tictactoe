342e2ca74aba33b577208afaeb0240426523c69c
Alex <weiru@hotmail.ca>
2022-07-03 17:22:24 -0400

    Add logic to check for winner

--- script.js
+++ script.js
@@ -21,30 +21,59 @@ const playGame = (() => {
     const { board } = gameBoard;
 
     let currentPiece = '';
+    let winner = '';
 
     const mark = (e) => {
         let desiredSquare = board[`${e.target.id}`];
         if (currentPiece === '') {
             currentPiece = playerOne.piece;
             if (desiredSquare === '') {
-                board.splice(e.target.id, 1, currentPiece)
+                board.splice(e.target.id, 1, currentPiece);
             }
         }
-        else if (currentPiece === playerOne.piece) {
+        else if (currentPiece === playerOne.piece && desiredSquare === '') {
             currentPiece = playerTwo.piece;
-            if (desiredSquare === '') {
-                board.splice(e.target.id, 1, currentPiece)
-            }
+            board.splice(e.target.id, 1, currentPiece);
+            winner = playerTwo.name;
+            console.log(currentPiece)
         }
-        else if (currentPiece === playerTwo.piece) {
+        else if (currentPiece === playerTwo.piece && desiredSquare === '') {
             currentPiece = playerOne.piece;
-            if (desiredSquare === '') {
-                board.splice(e.target.id, 1, currentPiece)
-            }
-        }
+            board.splice(e.target.id, 1, currentPiece);
+            winner = playerOne.name;
+            console.log(currentPiece)
+        };
 
         const { createBoard } = render;
         createBoard();
+        checkWinner();
+    };
+
+    function checkWinner() {
+        if (board[0] === board[1] && board[1] === board[2] && board[0] !== '') {
+            console.log(winner)
+        }
+        else if (board[3] === board[4] && board[4] === board[5] && board[3] != '') {
+            console.log(winner)
+        }
+        else if (board[6] === board[7] && board[7] === board[8] && board[6] != '') {
+            console.log(winner)
+        }
+        else if (board[0] === board[3] && board[3] === board[6] && board[0] != '') {
+            console.log(winner)
+        }
+        else if (board[1] === board[4] && board[4] === board[7] && board[1] != '') {
+            console.log(winner)
+        }
+        else if (board[2] === board[5] && board[5] === board[8] && board[2] != '') {
+            console.log(winner)
+        }
+        else if (board[0] === board[4] && board[4] === board[8] && board[0] != '') {
+            console.log(winner)
+        }
+        else if (board[2] === board[4] && board[4] === board[6] && board[2] != '') {
+            console.log(winner)
+        };
     }
 
     let selectedSquare = document.querySelectorAll('.gridBox');
