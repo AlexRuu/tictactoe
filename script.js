de18e8a64d2a29ff7d56d0ad77078ca6235ff8b1
Alex <weiru@hotmail.ca>
2022-07-06 21:58:41 -0400

    Clean up

--- script.js
+++ script.js
@@ -62,51 +62,51 @@ const playGame = (() => {
         if (board[0] === board[1] && board[1] === board[2] && board[0] !== '') {
             removeMove();
             currentPiece = '';
-            winnerMessage(winner, currentPiece);
+            winnerMessage(winner);
             return;
         }
         else if (board[3] === board[4] && board[4] === board[5] && board[3] != '') {
             removeMove();
             currentPiece = '';
-            winnerMessage(winner, currentPiece);
+            winnerMessage(winner);
             return;
         }
         else if (board[6] === board[7] && board[7] === board[8] && board[6] != '') {
             removeMove();
             currentPiece = '';
-            winnerMessage(winner, currentPiece);
+            winnerMessage(winner);
             return;
         }
         // Vertical Win
         else if (board[0] === board[3] && board[3] === board[6] && board[0] != '') {
             removeMove();
             currentPiece = '';
-            winnerMessage(winner, currentPiece);
+            winnerMessage(winner);
             return;
         }
         else if (board[1] === board[4] && board[4] === board[7] && board[1] != '') {
             removeMove();
             currentPiece = '';
-            winnerMessage(winner, currentPiece);
+            winnerMessage(winner);
             return;
         }
         else if (board[2] === board[5] && board[5] === board[8] && board[2] != '') {
             removeMove();
             currentPiece = '';
-            winnerMessage(winner, currentPiece);
+            winnerMessage(winner);
             return;
         }
         // Diagonal Win
         else if (board[0] === board[4] && board[4] === board[8] && board[0] != '') {
             removeMove();
             currentPiece = '';
-            winnerMessage(winner, currentPiece);
+            winnerMessage(winner);
             return;
         }
         else if (board[2] === board[4] && board[4] === board[6] && board[2] != '') {
             removeMove();
             currentPiece = '';
-            winnerMessage(winner, currentPiece);
+            winnerMessage(winner);
             return;
         }
         // Draw
@@ -118,7 +118,7 @@ const playGame = (() => {
                     if (counter === 9) {
                         winner = 'Draw';
                         currentPiece = '';
-                        winnerMessage(winner, currentPiece);
+                        winnerMessage(winner);
                         return;
                     };
                 };
